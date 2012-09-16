31008acc47dcbfe3185b35d7edad8185e884b82f
Mason Wendell <mason@canarypromo.com>
2012-09-16 01:08:14 -0400

    moving to a zen-based theme

---
+++ webroot/sites/all/themes/not-bbm/template.php
@@ -0,0 +1,7 @@
+<?php
+
+// function bbm_preprocess_page(&$variables) {
+//   print_r("your mom");
+//   drupal_add_js('http://use.typekit.com/gxt0pvy.js');
+//   drupal_add_js('//<![CDATA[ try{Typekit.load();}catch(e){} //]]>', 'inline');
+// }
